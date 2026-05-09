40afe082858336eb50a2aef3ed593f672a2df821
shifa-khan-05 <sheikhshifa748@gmail.com>
2026-05-09 10:44:11 +0530

    [Shifa Khan] : Modified post resource file for fix the post service

--- post-service/src/main/java/com/postservice/controller/PostResource.java
+++ post-service/src/main/java/com/postservice/controller/PostResource.java
@@ -39,7 +39,11 @@ public class PostResource {
 		dto.setCategoryId(categoryId);
 		dto.setTagIds(tagIds);
 
-		return new ResponseEntity<>(postService.createPostWithImage(dto, image), HttpStatus.CREATED);
+		try {
+			return new ResponseEntity<>(postService.createPostWithImage(dto, image), HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 	@PutMapping(value = "/update/{postId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
